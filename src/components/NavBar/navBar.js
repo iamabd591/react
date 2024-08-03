d58c122df0242e4768b57b9dd924e3b37e02a7b7
Abdul hanan butt <buttabdulhannan708@gmail.com>
2024-08-03 22:53:31 +0500

    navbar completed

---
+++ src/components/NavBar/navBar.js
@@ -0,0 +1,43 @@
+import "./NavBar.scss";
+import React, { useState } from "react";
+function NavBar() {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="logo" />
+          <span>RealEState</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/" className="signin">
+          Sign in
+        </a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/hamburger.png"
+            alt="hamburger"
+            onClick={() => setOpen(!open)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default NavBar;
